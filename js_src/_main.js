7246b9c456df2cc6eb690f77cda89a9d7f821a1b
rladbal0101 <rladbal0101@naver.com>
2017-08-07 18:24:18 +0900

    170807 추가

--- js_src/_main.js
+++ js_src/_main.js
@@ -2,3 +2,38 @@
  * Created by Administrator on 2017-08-04.
  */
 
+$(function(){
+
+  var scrollDelta = 0;
+
+  $(window).on('mousewheel', function(e){
+
+    if(e.originalEvent.wheelDelta < 0){
+
+      scrollDelta += e.originalEvent.wheelDelta;
+
+    } else {
+
+      scrollDelta += e.originalEvent.wheelDelta;
+
+    }
+    console.log( -1 * ( $('.menu-list').offset().left + $('.menu-list').width()));
+
+    if( scrollDelta >=0 ){
+
+      scrollDelta = 0;
+
+    } else if( scrollDelta <= -1 * ($('.menu-list').offset().left + $('.menu-list').width()) ){
+
+      scrollDelta = -1 *($('.menu-list').offset().left + $('.menu-list').width());
+
+    }
+
+    var tr = 'translateX(' + scrollDelta + 'px)';
+    $('.menu-list').css({
+      transform:tr
+    });
+
+  });
+
+});
