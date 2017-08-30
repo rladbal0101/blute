2dfcbd74deff97a767f2adc533ec01add1572e99
rladbal0101 <rladbal0101@naver.com>
2017-08-30 13:40:48 +0900

    170830 13:40

--- js_src/_common.js
+++ js_src/_common.js
@@ -10,6 +10,22 @@ $(function(){
 
   });
 
+  //$('.gnb-list-link').on('click', function(e){
+  //
+  //  e.preventDefault();
+  //
+  //  var href = $(this).attr('href');
+  //
+  //  var scrollPosition = $(href).offset().left;
+  //
+  //  // 주르륵 내려가는 효과
+  //  $('body').stop().animate({
+  //    scrollLeft : scrollPosition
+  //  });
+  //
+  //});
+
+
   // 마우스 오버시 중복 방지하기 위한 변수 선언
   var tID1, tID2;
 
@@ -18,7 +34,6 @@ $(function(){
 
     $(this).children('.small-text.change').css({display:'none'});
     $(this).children('.hidden-text-wrap').addClass('on');
-    $(this).children('.popup-btn-more').addClass('on');
 
     tID1 = setTimeout(function(){
       // 왼쪽,오른쪽줄(시작) 모션적용 클래스
@@ -32,7 +47,7 @@ $(function(){
       $('.hidden-text-wrap').addClass('frame2');
       $('.hidden-text').addClass('frame4');
 
-    }, 500);
+    }, 300);
 
   });
 
@@ -50,7 +65,7 @@ $(function(){
 
   });
 
-  $('.popup-btn-more').on('click', function(){
+  $('.hidden-text-wrap.more').on('click', function(){
 
     $('.popup-height').removeClass('on');
     $('.popup-width').removeClass('on');
